4ef8ba9dd1494409e03ec84a4424243076c65c98
Omer <Omertorrents@gmail.com>
2016-09-22 22:49:43 +0300

    fixing last picture parsing

--- http/mainApp.py
+++ http/mainApp.py
@@ -23,7 +23,7 @@ class AppHandler(tornado.web.RequestHandler):
     def get(self):
         t = instagramConnectionFacade(self.get_argument("code"),"5f46ab2c0ce24bdaa966b3ea9b1b9b2a", "8c5523d19c604c0dac2c66946083a5b4",
                                       "http://ec2-54-244-111-228.us-west-2.compute.amazonaws.com/app")
-        l = t.getRecentPhotos(20)
+        l = t.getRecentPhotos(100)
         print("The photos are")
         print(l)
         print("The len of photos is: %d"%len(l))
