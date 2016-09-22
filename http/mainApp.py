7fb81dbab9f0af777014ea94297f5b7f556055ea
Omer <Omertorrents@gmail.com>
2016-09-22 21:41:12 +0300

    trying to read comment

--- http/mainApp.py
+++ http/mainApp.py
@@ -23,9 +23,8 @@ class AppHandler(tornado.web.RequestHandler):
     def get(self):
         t = instagramConnectionFacade(self.get_argument("code"),"5f46ab2c0ce24bdaa966b3ea9b1b9b2a", "8c5523d19c604c0dac2c66946083a5b4",
                                       "http://ec2-54-244-111-228.us-west-2.compute.amazonaws.com/app")
-        # user = t.getUser()
+        l = t.getRecentPhotos(1)
         self.render("src/LikelyMain.html", uname=t.getUser(), pProfile=t.getProfilePic())
-        print(t.getUser(), t.getProfilePic())
 
 class UploadHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
