1ffbf526b565d54ac4f19496c74f30bb01fdd833
Omer <Omertorrents@gmail.com>
2016-09-22 20:38:49 +0300

    File Upload

--- http/mainApp.py
+++ http/mainApp.py
@@ -20,6 +20,9 @@ class AppHandler(tornado.web.RequestHandler):
         print(t.getUser(), t.getProfilePic())
 
 class UploadHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+
     def post(self):
         file1 = self.request.files['file1'][0]
         original_fname = file1['filename']
