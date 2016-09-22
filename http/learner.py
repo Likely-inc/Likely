7496c439ba03df954ca7ec142b6cc0babe8fd491
Erez Levanon <erez.levanon@mail.huji.ac.il>
2016-09-23 01:37:05 +0300

    print image url

--- http/learner.py
+++ http/learner.py
@@ -1,5 +1,5 @@
 from sklearn import linear_model
-# import imageClassifier
+import imageClassifier
 import sklearn.feature_extraction
 from datetime import datetime
 
@@ -11,11 +11,14 @@ def train(list_of_dicts, new_photo_dict):
         this_time = list_of_dicts[i]['created_time']
         dow = "DOW_" + datetime.fromtimestamp(float(this_time)).strftime("%A")
         curdict = {list_of_dicts[i]["filter"]: 1, list_of_dicts[i]["location"]: 1, dow: 1}
+        print(imageClassifier.getImageFeatures(list_of_dicts[i]["image_link"]))
+        # googles = imageClassifier.getImageFeatures(list_of_dicts[i]["image_link"])
+        # for key in googles.keys():
+        #     curdict[key] = 1
         training_vects.append(curdict)
 
     vectorizer = sklearn.feature_extraction.DictVectorizer()
-
-
+    vectorizer.fit_transform(list_of_dicts)
 
     print(training_vects)
     # return predict(predictor, new_vec)
