cbebedeb81e6577d1547f98a37ba8036ee79aeff
Omer <Omertorrents@gmail.com>
2016-09-22 15:01:26 +0300

    1123123

--- http/InstaAPI.py
+++ http/InstaAPI.py
@@ -1,9 +1,33 @@
 
 
 from instagram.client import InstagramAPI
+from instagram.oauth2 import OAuth2AuthExchangeRequest
+import urllib,json
+user_agent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
+headers = {'User-Agent': user_agent,
+           "Content-type": "application/x-www-form-urlencoded"
+           }
+
 class instagramConnectionFacade:
     __api = None
     def __init__(self, client_id, appId, appSecret, URI):
+        url = "https://api.instagram.com/oauth/access_token"
+        values = {
+            'client_id':appId,
+            'client_secret':appSecret,
+            'grant_type':'authorization_code',
+            'redirect_uri':URI,
+            'code':client_id
+        }
+
+        try:
+            data = urllib.urlencode(values)
+            req = urllib.Request(url, data, headers)
+            response = urllib.urlopen(req)
+            result = response.read()
+            dataObj = json.loads(result)
+        except Exception:
+            print("Yo")
         self.__api = InstagramAPI(
             client_id=appId,
             client_secret=appSecret,
