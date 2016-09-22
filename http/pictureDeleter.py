fdb9146db52d5c3a53a96e41018c7a5b3a5e5933
Omer <Omertorrents@gmail.com>
2016-09-23 02:45:16 +0300

    pythonDeleter

--- http/pictureDeleter.py
+++ http/pictureDeleter.py
@@ -1,11 +1,18 @@
 import time
 import os, os.path
 
-for root, _, files in os.walk("upload"):
-    for f in files:
-        fullpath = os.path.join(root, f)
-        print(fullpath)
-        tTime = os.path.getmtime(fullpath)/60/60/60/60
-        if(tTime < 113.779):
-            os.remove(fullpath)
-            print("Delete "+fullpath)
+
+def deleteSomeFiles():
+    for root, _, files in os.walk("upload"):
+        for f in files:
+            fullpath = os.path.join(root, f)
+            print(fullpath)
+            tTime = os.path.getmtime(fullpath)/60/60/60/60
+            if(tTime < 113.779):
+                os.remove(fullpath)
+                print("Delete "+fullpath)
+
+
+while(True):
+    time.sleep(20)
+    deleteSomeFiles()
