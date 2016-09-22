a312ba5d98ff5533f2fd1244dd68da1c1c5d91ca
Omer <Omertorrents@gmail.com>
2016-09-22 14:38:04 +0300

    1123123

--- http/mainApp.py
+++ http/mainApp.py
@@ -14,7 +14,7 @@ class MainHandler(tornado.web.RequestHandler):
 class AppHandler(tornado.web.RequestHandler):
     def get(self):
         t = instagramConnectionFacade(self.get_argument("code"),"5f46ab2c0ce24bdaa966b3ea9b1b9b2a", "8c5523d19c604c0dac2c66946083a5b4",
-                                      self.get_argument("Host")+"/app")
+                                      "http://likely.uk.to/app")
         user = t.getUser()
         print(user)
 
