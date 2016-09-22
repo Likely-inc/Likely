0cf62383bb469637afd5b4671d0355a019ef7ea3
Omer <Omertorrents@gmail.com>
2016-09-22 15:22:24 +0300

    1123123

--- http/InstaAPI.py
+++ http/InstaAPI.py
@@ -3,6 +3,7 @@
 from instagram.client import InstagramAPI
 from instagram.oauth2 import OAuth2AuthExchangeRequest
 import requests
+import json
 user_agent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
 headers = {'User-Agent': user_agent,
            "Content-type": "application/x-www-form-urlencoded"
@@ -10,6 +11,9 @@ headers = {'User-Agent': user_agent,
 
 class instagramConnectionFacade:
     __api = None
+    __uName = None
+    __aToken = None
+    __pPicture = None
     def __init__(self, client_id, appId, appSecret, URI):
         url = "https://api.instagram.com/oauth/access_token"
         values = {
@@ -19,11 +23,12 @@ class instagramConnectionFacade:
             'redirect_uri':URI,
             'code':client_id
         }
-
         try:
             r = requests.post(url, data=values)
-            print(r.status_code, r.reason)
-            print(r.text[:300])
+            js = json.dump(r.text[:300])
+            self.__uName = js["full_name"]
+            self.__aToken = js["access_token"]
+            self.__pPicture = js["profile_picture"]
         except Exception as e:
             print(e.args)
         # self.__api = InstagramAPI(
@@ -32,5 +37,7 @@ class instagramConnectionFacade:
         #     redirect_uri=URI, access_token=client_id)
 
     def getUser(self):
-        popular_media = self.__api.media_popular(count=20)
-        return [elem.images['standard_resolution'].url for elem in popular_media]
+        return self.__uName
+
+    def getProfilePic(self):
+        return self.__pPicture
