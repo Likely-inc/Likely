5d3645bc4c85c33ff48740e18eabcf6be4b6a3bb
Erez Levanon <erez.levanon@mail.huji.ac.il>
2016-09-23 02:52:57 +0300

    nice print for training vec

--- http/learner.py
+++ http/learner.py
@@ -13,7 +13,6 @@ def train(list_of_dicts, new_photo_dict):
         dow = "DOW_" + datetime.fromtimestamp(float(this_time)).strftime("%A")
         curdict = {list_of_dicts[i]["filter"]: 1, list_of_dicts[i]["location"]: 1, dow: 1}
         googles = imageClassifier.getImageFeatures(list_of_dicts[i]["image_link"])
-        # print(googles)
         for key in googles.keys():
             for label in googles[key]:
                 if label in ["blueMean", "redMean"]:
@@ -26,19 +25,24 @@ def train(list_of_dicts, new_photo_dict):
     # vectorizer = sklearn.feature_extraction.DictVectorizer()
     # vectorizer.fit_transform(list_of_dicts)
 
-    print(training_vects)
+    print_oneline(training_vects)
 
     vectorizer = sklearn.feature_extraction.DictVectorizer()
     vectorizer.fit_transform(training_vects)
 
     data = vectorizer.fit(training_vects)
 
-    predictor = linear_model.RidgeCV()
-    predictor.fit(data, labels)
+    # predictor = linear_model.RidgeCV()
+    # predictor.fit(data, labels)
 
 
     # return predict(predictor, new_vec)
     return 37
+
+
+def print_oneline(training_vecs):
+    for vec in training_vecs:
+        print(vec)
 
 def predict(predictor, new_vector):
     return predictor.predict(new_vector)[0]
