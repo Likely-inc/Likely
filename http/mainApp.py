606404ffab7c24018d497af496db51685c7e0959
Omer <Omertorrents@gmail.com>
2016-09-22 22:19:09 +0300

    reseting python

--- http/mainApp.py
+++ http/mainApp.py
@@ -14,9 +14,9 @@ class MainHandler(tornado.web.RequestHandler):
 
 class ResetHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Restart the server 2")
+        self.write("Restart the server 3")
         print("Killing python")
-        os.popen("./PullScriptForOmer")
+        os.system("./PullScriptForOmer")
 
 
 class AppHandler(tornado.web.RequestHandler):
