601590c4f1192964fc41f0b497f9e94bc1416462
Omer <Omertorrents@gmail.com>
2016-09-23 02:33:30 +0300

    pythonDeleter

--- http/pictureDeleter.py
+++ http/pictureDeleter.py
@@ -3,13 +3,5 @@ import os
 
 p = os.listdir("upload")
 for d in os.listdir("upload"):
-    if(os.path.isfile(d)):
-        print(d)
-        print("Is file!!")
-    elif(os.path.isdir(d)):
-        print(d)
-        print("Is dir!!")
-        for elem in os.listdir(d):
-            print(d)
-            print("Is file!!")
+    print(d)
 
