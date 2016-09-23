530a6f3cb14b88587b7e0c9aa5d79d4a95664e69
Erez Levanon <erez.levanon@mail.huji.ac.il>
2016-09-23 03:12:45 +0300

    add hour of day

--- http/learner.py
+++ http/learner.py
@@ -10,8 +10,11 @@ def train(list_of_dicts, new_photo_dict):
     labels = []
     for i in range (len(list_of_dicts)):
         this_time = list_of_dicts[i]['created_time']
-        dow = "DOW_" + datetime.fromtimestamp(float(this_time)).strftime("%A")
+        format_time = datetime.fromtimestamp(float(this_time)).strftime("%A %H")
+        dow = "DOW_" + format_time.split()[0]
         curdict = {list_of_dicts[i]["filter"]: 1, list_of_dicts[i]["location"]: 1, dow: 1}
+        for hour, w in [(-1,0.25), (0, 0.5), (1, 0.25)]:
+            curdict["hod_" + str((int(format_time.split()[0])+hour) % 24)] = w
         googles = imageClassifier.getImageFeatures(list_of_dicts[i]["image_link"])
         for key in googles.keys():
             for label in googles[key]:
