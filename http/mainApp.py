1d065c4570d1af302697ff209e76714b8dfeb987
Omer <Omertorrents@gmail.com>
2016-09-23 05:36:29 +0300

    logistics

--- http/mainApp.py
+++ http/mainApp.py
@@ -44,7 +44,7 @@ class AppHandler(tornado.web.RequestHandler):
     def get(self):
 
         t = instagramConnectionFacade(self.get_argument("code"),"5f46ab2c0ce24bdaa966b3ea9b1b9b2a", "8c5523d19c604c0dac2c66946083a5b4",
-                                      "http://ec2-54-244-111-228.us-west-2.compute.amazonaws.com/app")
+                                      "http://likely.uk.to/app")
         ipAndCodes[self.request.remote_ip] = t
         self.render("src/LikelyMain.html", uname=t.getUser(), pProfile=t.getProfilePic())
 
